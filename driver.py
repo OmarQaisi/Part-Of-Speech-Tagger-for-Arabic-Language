83b1f24eb0a7d36cf93f6936cea999dd3cbc89c7
Marwan998 <m.alramahi98@gmail.com>
2019-11-03 19:37:28 +0200

    added tagger

--- driver.py
+++ driver.py
@@ -1,7 +1,14 @@
 from tokenizer import Tokenizer
+from tagger import Tagger
 
 test = "Corpus\\Technology\\تقنية بلوك تشين2.txt"
 token = Tokenizer(test)
 result = token.tokenize()
 for word in result:
     print(word)
+tagger = Tagger()
+tags = tagger.tag(result)
+for t,v in tags.items():
+    print(t,v)
+print(len(result))
+print(len(tags))
