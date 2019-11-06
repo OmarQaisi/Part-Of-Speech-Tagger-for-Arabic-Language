3416d0dcd3f0d5f4e2b584bb5168c547ea8d4570
Marwan998 <m.alramahi98@gmail.com>
2019-11-06 14:50:17 +0200

    Finished sports golden corpus

--- driver.py
+++ driver.py
@@ -2,7 +2,7 @@ from tokenizer import Tokenizer
 from tagger import Tagger
 
 
-test = "Corpus\\Sports\\ظاهرة العنف والتعصب الرياضي.txt"
+test = "Corpus\\Sports\\الرياضة و شهر رمضان المبارك.txt"
 
 token = Tokenizer(test)
 result = token.tokenize()
@@ -14,10 +14,3 @@ for i in range(0,len(result)):
 print(len(result))
 print(len(tags))
 print(tags)
-import pandas as pd
-list = [None] * len(result)
-for i in range(len(result)):
-    list[i] = ([result[i], 'N'])
-
-df = pd.DataFrame(list)
-df.to_csv("ظاهرة العنف والتعصب الرياضي.csv", index=False, header=False)
